4eadb5475c319954abd025c88e60adf54b7e51dd
Konstantin0812 <titi081203@gmail.com>
2025-08-31 12:18:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,18 @@
-﻿int totalSteps = 0;
+﻿string destination = Console.ReadLine();
 
-string input = Console.ReadLine();
-while (input != "Going home")
+while (destination != "End")
 {
-    int steps = int.Parse(input);
-    totalSteps += steps;
-    if(totalSteps >= 10000)
+    double moneyForTrip = double.Parse(Console.ReadLine());
+
+    double savedMoney = 0;
+    while (savedMoney < moneyForTrip)
     {
-        break;
+        double currentMoney = double.Parse(Console.ReadLine());
+        savedMoney += currentMoney;
     }
-  input = Console.ReadLine();
-}
-if(totalSteps < 10000)
-{
-    int stepsFromTheParkToHome = int.Parse(Console.ReadLine());
-    totalSteps += stepsFromTheParkToHome;
-}
-if (totalSteps >= 10000)
-{
-    Console.WriteLine($"Goal reached! Good job!");
-    Console.WriteLine($"{totalSteps - 10000} steps over the goal!");
-}
-else
-{
-    Console.WriteLine($"{10000 - totalSteps} more steps to reach goal.");
+
+    Console.WriteLine($"Going to {destination}!");
+
+    destination = Console.ReadLine();
+    
 }
