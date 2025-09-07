882ebc94dba3e995cc37a26e3778220bd265e9cf
Konstantin0812 <titi081203@gmail.com>
2025-09-07 18:23:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,5 @@
-﻿int n = int.Parse(Console.ReadLine());
-int presentationsCount = 0;
-double totalSumOfGrades = 0;
-string command;
-while ((command = Console.ReadLine()) != "Finish")
-{
-    string presentationName = command;
-    presentationsCount++;
-    double sumOfGrade = 0;
-    for(int i = 0; i < n; i++)
-    { 
-        double grade = double.Parse(Console.ReadLine());
-        sumOfGrade += grade;
-        totalSumOfGrades += grade;
-    }
-    Console.WriteLine($"{presentationName} - {sumOfGrade / n:f2}. ");
+﻿double usd = double.Parse(Console.ReadLine());
+double bgn = usd * 1.79549; 
+Console.WriteLine(bgn);
 
-}
-Console.WriteLine($"Student's final assessment is {totalSumOfGrades / (presentationsCount * n):f2}. ");
+
